1d3bbd58613dc21efa637b968a70f7d8da667850
Aayush Shrestha <Aayushshrestha2003@gmail.com>
2025-03-09 12:01:15 +0545

    - added tab bars icons - removed unnecessary headers

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,33 @@
 import { Tabs } from 'expo-router';
+import Feather from '@expo/vector-icons/Feather'
 
 export default function TabLayout() {
     return (
         <Tabs>
-            <Tabs.Screen name="(home)" />
-            <Tabs.Screen name="history" />
-            <Tabs.Screen name="utilities" />
+            <Tabs.Screen name="(home)"
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <Feather name="home" color={color} size={size} />
+                    ),
+                    tabBarLabel: "Home",
+                }} />
+            <Tabs.Screen name="history"
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <Feather name="file-text" color={color} size={size} />
+                    ),
+                    tabBarLabel: "Transactions",
+                }} />
+            <Tabs.Screen name="utilities"
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <Feather name="tool" color={color} size={size} />
+                    ),
+                    tabBarLabel: "Utilities",
+                }} />
         </Tabs>
     );
 }
