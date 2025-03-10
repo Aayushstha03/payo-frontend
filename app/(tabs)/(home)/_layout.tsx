16ba52b2eafb9e2b40397fb2046bb4fd950667b8
Aayush Shrestha <Aayushshrestha2003@gmail.com>
2025-03-10 21:38:18 +0545

    profile+setting page, removed settings page

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -10,9 +10,6 @@ export default function RootLayout() {
             <Stack.Screen name="profile" options={{
                 headerShown: false,
             }} />
-            <Stack.Screen name="settings" options={{
-                headerShown: false,
-            }} />
         </Stack>
     );
 }
